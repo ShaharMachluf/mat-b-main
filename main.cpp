f8fba47bae15cbc6e33b746aded66d7994006916
ShaharMachluf <shahar6261@gmail.com>
2022-03-11 16:56:47 +0200

    print mat

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ int main(){
     while (true){
         int cols = 0, rows = 0;
         char first = 'a', second = 'b', choice = 'A';
+        string mat = "";
         cout<<"To make a mat please press 'A', to quit press 'B'"<<endl;
         if(choice == 'A'){
             cout<<"enter two numbers and two symbols for the mat (seperate them using space)"<<endl;
             cin>>cols>>rows>>first>>second;
-            ariel::mat(cols, rows, first, second);
+            mat = ariel::mat(cols, rows, first, second);
+            cout<<mat<<endl; 
         } else {
             return 0;
         }

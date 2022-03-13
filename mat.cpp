aac14a336d1e025488c352d0cd5b656a3b10e83e
ShaharMachluf <shahar6261@gmail.com>
2022-03-13 10:04:30 +0200

    made matrix

--- mat.cpp
+++ mat.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "mat.hpp"
 
 using namespace std;
@@ -14,13 +15,52 @@ bool check_exceptions(int cols, int rows, char first, char second){
     } else if(first == ' ' || first == '\n' || first == '\t' || second == ' ' || second == '\n' || second == '\t'){
         throw invalid_argument("The symbols must not be empty symbols");
         return false;
+    } else if(cols < 33 || cols > 126 || rows < 33 || rows > 126){
+        throw invalid_argument("The symbols must be printable");
+        return false;
     }
     return true;
 }
 
 namespace ariel{
     string mat(int cols, int rows, char first, char second){
-        bool valid = check_exceptions(cols, rows, first, second);
+        int i = 0, j=0;
+        char sym = first;
+        bool valid = check_exceptions(cols, rows, first, second); //make sure the input is valid
+        if(!valid){
+            return "";
+        }
+        vector<vector<char>> vec( rows , vector<char> (cols));
+        while(i*2 <= rows && i*2 <= cols){ //go through the 2D vector by "frames" going from the outside in
+            if(i%2 == 0){ //choose the current symbol
+                sym = first;
+            } else{
+                sym = second;
+            }
+            for(j = i; j < cols-i; j++){ //go through the i-th and the rows-i-th row
+                vec[i][j] = sym;
+                vec[rows-i-1][j] = sym;
+            }
+            for(j = i; j < rows-i; j++){ //go through the i-th and the cols-i-th column
+                vec[j][i] = sym;
+                vec[j][cols-i-1] = sym;
+            }
+            i++;
+        }
+        if(i%2 == 0){ //choose the current symbol
+            sym = first;
+        } else{
+            sym = second;
+        }
+        if(i*2+1 == rows){ //there is more columns then rows
+            for(j = i; j < cols-i; j++){
+                vec[i][j] = sym;
+            }
+        } else{
+            for(j = i; j < rows-i; j++){ //there is more rows then columns 
+                vec[j][i] = sym;
+            }
+        }
         return ("ok");
     }
 };
